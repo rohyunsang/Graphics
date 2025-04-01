6294ec8a43dda40b81c7c4b1a2284d5efda9a11d
Roh <82229769+rohyunsang@users.noreply.github.com>
2025-04-01 19:17:11 +0900

    PNG Loader

--- Graphics/Game.cpp
+++ Graphics/Game.cpp
@@ -23,6 +23,8 @@ void Game::Init(HWND hwnd)
 	CreateVS();
 	CreateInputLayout();
 	CreatePS();
+
+	CreateSRV();
 }
 
 void Game::Update()
@@ -40,6 +42,7 @@ void Game::Render()
 
 		// IA	Input Assembler
 		_deviceContext->IASetVertexBuffers(0, 1, _vertexBuffer.GetAddressOf(), &stride, &offset);
+		_deviceContext->IASetIndexBuffer(_indexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
 		_deviceContext->IASetInputLayout(_inputLayout.Get());
 		_deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
@@ -50,11 +53,12 @@ void Game::Render()
 		
 		// PS	Pixel Shader
 		_deviceContext->PSSetShader(_pixelShader.Get(), nullptr, 0);
+		_deviceContext->PSSetShaderResources(0, 1, _shaderResourceView.GetAddressOf());
 
 		// OM	Output Merger
 
-		_deviceContext->Draw(_vertices.size(), 0);
-
+		// _deviceContext->Draw(_vertices.size(), 0);
+		_deviceContext->DrawIndexed(_indices.size(), 0, 0);
 
 	}
 
@@ -134,17 +138,26 @@ void Game::SetViewport()
 void Game::CreateGeometry()
 {
 	{
-		_vertices.resize(3);
+		_vertices.resize(4);
+
+		// 13
+		// 02 
 
 		_vertices[0].position = Vec3(-0.5f, -0.5f, 0.f);
-		_vertices[0].color = Color(1.f, 0.f, 0.f, 1.f);
+		//_vertices[0].color = Color(1.f, 0.f, 0.f, 1.f);
+		_vertices[0].uv = Vec2(0.f, 1.f);
 
-		_vertices[1].position = Vec3(0.f, 0.5f, 0.f);
-		_vertices[1].color = Color(1.f, 0.f, 0.f, 1.f);
+		_vertices[1].position = Vec3(-0.5f, 0.5f, 0.f);
+		//_vertices[1].color = Color(1.f, 1.f, 0.f, 1.f);
+		_vertices[1].uv = Vec2(0.f, 0.f);
 
 		_vertices[2].position = Vec3(0.5f, -0.5f, 0.f);
-		_vertices[2].color = Color(1.f, 0.f, 0.f, 1.f);
+		//_vertices[2].color = Color(1.f, 0.f, 1.f, 1.f);
+		_vertices[2].uv = Vec2(1.f, 1.f);
 
+		_vertices[3].position = Vec3(0.5f, 0.5f, 0.f);
+		//_vertices[3].color = Color(1.f, 0.f, 0.f, 1.f);
+		_vertices[3].uv = Vec2(1.f, 0.f);
 
 		D3D11_BUFFER_DESC desc;
 		ZeroMemory(&desc, sizeof(desc));
@@ -156,15 +169,39 @@ void Game::CreateGeometry()
 		ZeroMemory(&data, sizeof(data));
 		data.pSysMem = _vertices.data();
 
-		_device->CreateBuffer(&desc, &data, _vertexBuffer.GetAddressOf());
+		HRESULT hr = _device->CreateBuffer(&desc, &data, _vertexBuffer.GetAddressOf());
+		CHECK(hr);
 	}
+
+	// index  
+	{
+		_indices = { 0,1,2,2,1,3 };
+	}
+
+	// index buffer
+	{
+		D3D11_BUFFER_DESC desc;
+		ZeroMemory(&desc, sizeof(desc));
+		desc.Usage = D3D11_USAGE_IMMUTABLE;
+		desc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		desc.ByteWidth = (uint32)(sizeof(uint32) * _indices.size());
+
+		D3D11_SUBRESOURCE_DATA data;
+		ZeroMemory(&data, sizeof(data));
+		data.pSysMem = _indices.data();
+
+		HRESULT hr = _device->CreateBuffer(&desc, &data, _indexBuffer.GetAddressOf());
+		CHECK(hr);
+	}
+
+
 }
 
 void Game::CreateInputLayout()
 {
 	D3D11_INPUT_ELEMENT_DESC layout[] = {
 		{"POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
-		{"COLOR",0,DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
+		{"TEXCOORD",0,DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
 	};
 	
 	const int32 count = sizeof(layout) / sizeof(D3D11_INPUT_ELEMENT_DESC);
@@ -186,6 +223,21 @@ void Game::CreatePS()
 	HRESULT hr = _device->CreatePixelShader(_psBlob->GetBufferPointer(), _psBlob->GetBufferSize(), nullptr, _pixelShader.GetAddressOf());
 	CHECK(hr);
 	
+}
+
+void Game::CreateSRV()
+{
+	DirectX::TexMetadata md;
+	DirectX::ScratchImage img;
+
+	HRESULT hr = ::LoadFromWICFile(L"winter.png", WIC_FLAGS_NONE, &md, img);
+	CHECK(hr);
+
+	hr = ::CreateShaderResourceView(_device.Get(), img.GetImages(), img.GetImageCount(), md, _shaderResourceView.GetAddressOf());
+	CHECK(hr);
+
+
+
 }
 
 void Game::LoadShaderFromFile(const wstring& path, const string& name, const string& version, ComPtr<ID3DBlob>& blob)
--- Graphics/Game.h
+++ Graphics/Game.h
@@ -29,6 +29,8 @@ private:
 	void CreateVS();
 	void CreatePS();
 
+	void CreateSRV();
+
 	void LoadShaderFromFile(const wstring& path, const string& name, const string& version, ComPtr<ID3DBlob>& blob);
 
 private:
@@ -54,6 +56,8 @@ private:
 private:
 	vector<Vertex> _vertices;
 	ComPtr<ID3D11Buffer> _vertexBuffer = nullptr;
+	vector<uint32> _indices;
+	ComPtr<ID3D11Buffer> _indexBuffer = nullptr;
 	ComPtr<ID3D11InputLayout> _inputLayout = nullptr;
 
 	// VS
@@ -63,6 +67,9 @@ private:
 	// PS
 	ComPtr<ID3D11PixelShader> _pixelShader = nullptr;
 	ComPtr<ID3DBlob> _psBlob = nullptr;
+
+	// SRV
+	ComPtr<ID3D11ShaderResourceView> _shaderResourceView;
 
 
 };
